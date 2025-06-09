dab17cb9ee526f8fcee3912b4c8185271d494f37
shv187 <shv187@tutanota.com>
2025-06-10 00:16:11 +0200

    Adjust example

--- example/example.cpp
+++ example/example.cpp
@@ -13,21 +13,12 @@ int main() {
     overlay.run([](of::overlay_t* ov) {
         POINT pt{};
         GetCursorPos(&pt);
-        // ov->draw_rect_filled(pt.x, pt.y, 100, 100, 255, 255, 255, 100);
-        //
-        //
-        static POINT position{};
-        float smoothing_speed{ 25.f };
 
-        static POINT current_position{};
+        const auto offset = 50;
 
-        float delta_time = 1.f;
+        pt.x += offset;
+        pt.y += offset;
 
-        position.x = pt.x;
-        position.y = pt.y;
-        current_position.x = std::lerp<float>(static_cast<float>(current_position.x), static_cast<float>(position.x), std::clamp<float>(delta_time * smoothing_speed, 0.f, 1.f));
-        current_position.y = std::lerp<float>(static_cast<float>(current_position.y), static_cast<float>(position.y), std::clamp<float>(delta_time * smoothing_speed, 0.f, 1.f));
-
-        ov->draw_text("hi :3", current_position.x, current_position.y, 255, 255, 255, 255, true);
+        ov->draw_text("hi :3", pt.x, pt.y, 255, 255, 255, 255, true);
     });
 }
